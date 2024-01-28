7fba8f43503fa4ce5b9dac3a9e855ec698c11f82
Myk <89474773+mykcryptodev@users.noreply.github.com>
2024-01-28 13:20:28 -0500

    personalize cast

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import type { Metadata } from 'next';
 
 const frameMetadata = getFrameMetadata({
   buttons: ['Next image'],
-  image: 'https://zizzamia.xyz/park-1.png',
+  image: 'https://raw.seadn.io/files/47f8062e6daf87a5f3fe3de098f6b171.png',
   post_url: 'https://zizzamia.xyz/api/frame',
 });
 
 export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
+  title: 'myk.eth',
   description: 'LFG',
   openGraph: {
-    title: 'zizzamia.xyz',
+    title: 'myk.eth',
     description: 'LFG',
-    images: ['https://zizzamia.xyz/park-1.png'],
+    images: ['https://raw.seadn.io/files/f3cb5c80c96cea79441a15f06ee291c5.png'],
   },
   other: {
     ...frameMetadata,
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>zizzamia.xyz</h1>
+      <h1>myk.eth</h1>
     </>
   );
 }
